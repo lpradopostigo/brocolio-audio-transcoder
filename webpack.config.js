c99a6fc0861de267698651177cef56a0cb909f0b
Luis Prado <lpradopostigo@gmail.com>
2021-03-25 14:22:52 -0500

    folder structule

--- webpack.config.js
+++ webpack.config.js
@@ -4,12 +4,12 @@ const path = require("path");
 module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html")
+      template: path.resolve(__dirname, "src", "index.html")
     })
   ],
   entry: { index: path.resolve(__dirname, "src", "index.js") },
   output: {
-    path: path.resolve(__dirname, "build")
+    path: path.resolve(__dirname, "dist")
   },
   mode: 'development',
   module: {
